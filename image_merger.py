fde8e3713da33cd0ff91c4df4fda33e79103358a
emi <esauvisky@gmail.com>
2024-10-06 15:19:53 -0300

    much faster merger

--- image_merger.py
+++ image_merger.py
@@ -1,6 +1,6 @@
+import glob
+import os
 import time
-from functools import lru_cache
-from multiprocessing import Pool
 
 import numpy as np
 from PIL import Image
@@ -9,65 +9,61 @@ from loguru import logger
 
 class ImageMerger:
     @staticmethod
-    def find_image_overlap(base_array, new_array, threshold=0.995):
-        base_array_tuple = tuple(map(tuple, base_array))
-        new_array_tuple = tuple(map(tuple, new_array))
-        base_array = np.array(base_array_tuple)
-        new_array = np.array(new_array_tuple)
-
+    def find_image_overlap(base_array, new_array, threshold=0.9):
         logger.debug(f"Entering find_image_overlap with threshold {threshold}")
-        start_time = time.time()
 
-        height_base, width, _ = base_array.shape
+        height_base, width = base_array.shape
         height_new = new_array.shape[0]
 
-        # Vectorized comparison
         shifts = np.arange(-height_new + 1, height_base)
-        match_percentages = np.zeros_like(shifts, dtype=float)
+        match_percentages = np.zeros(shifts.shape[0], dtype=np.float32)
 
-        for i, shift in enumerate(shifts):
+        for i in range(shifts.shape[0]):
+            shift = shifts[i]
             start_base = max(0, shift)
             end_base = min(height_base, shift + height_new)
             start_new = max(0, -shift)
-            end_new = start_new + (end_base-start_base)
+            end_new = start_new + (end_base - start_base)
 
             if end_base - start_base > 0:
                 base_overlap = base_array[start_base:end_base]
                 new_overlap = new_array[start_new:end_new]
 
-                # Calculates which rows from the new image match the
-                # rows from the base image within the overlapping part
-                # Use axis=(2) to compare pixel by pixel instead row by row
-                matching_rows = np.all(base_overlap == new_overlap, axis=(1, 2))
-
-                match_percentages[i] = np.mean(matching_rows)
+                # Calculate matching percentage
+                match_percentages[i] = np.mean(base_overlap == new_overlap)
+                if match_percentages[i] == 1:
+                    logger.info(f"Match percentage at shift {shift} is 100%, returning early")
+                    return shift, match_percentages[i]
 
         best_match_index = np.argmax(match_percentages)
         best_match_percentage = match_percentages[best_match_index]
         best_shift = shifts[best_match_index] if best_match_percentage >= threshold else None
 
         end_time = time.time()
-        logger.debug(f"Exiting find_image_overlap. Time taken: {end_time - start_time:.4f} seconds")
         return best_shift, best_match_percentage
 
     @staticmethod
-    def merge_images_vertically(base_img, new_img, threshold=0.995):
+    def merge_images_vertically(base_img, new_img, threshold=0.9):
         logger.debug("Entering merge_images_vertically")
+
+        # Convert images to grayscale to reduce data size
+        base_img_gray = base_img.convert('L')
+        new_img_gray = new_img.convert('L')
+
         base_array = np.array(base_img)
         new_array = np.array(new_img)
+        base_array_gray = np.array(base_img_gray)
+        new_array_gray = np.array(new_img_gray)
 
-        if base_img.mode != new_img.mode:
-            raise ValueError(f"Image modes don't match: {base_img.mode} vs {new_img.mode}")
-
-        if base_array.shape[1] != new_array.shape[1]:
+        if base_array_gray.shape[1] != new_array_gray.shape[1]:
             raise ValueError("Images must have the same width")
 
-        shift, match_percentage = ImageMerger.find_image_overlap(base_array, new_array, threshold)
+        shift, match_percentage = ImageMerger.find_image_overlap(base_array_gray, new_array_gray, threshold)
 
         if shift is not None:
-            logger.info(f"Overlap detected at shift {shift} with match percentage {match_percentage:.2%}")
+            logger.success(f"Overlap detected at shift {shift} with match percentage {match_percentage:.2%}")
 
-            height_base, width, _ = base_array.shape
+            height_base = base_array.shape[0]
             height_new = new_array.shape[0]
 
             if shift < 0:
@@ -79,15 +75,16 @@ class ImageMerger:
 
             return Image.fromarray(merged_array)
         else:
-            logger.info("No overlap detected above the threshold, returning original base image")
+            logger.warning("No overlap detected above the threshold, returning original base image")
             return base_img
 
     @staticmethod
     def process_single_image(args):
         base_img_path, new_img_path, threshold = args
-        try:
-            base_img = Image.open(base_img_path)
-            new_img = Image.open(new_img_path)
+        base_img = Image.open(base_img_path)
+        new_img = Image.open(new_img_path)
+        return ImageMerger.merge_images_vertically(base_img, new_img, threshold)
+
 
 
 
