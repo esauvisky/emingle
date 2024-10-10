13e0ae93dee79c1f418587c573dab03ec2d698e8
emi <esauvisky@gmail.com>
2024-10-10 16:51:52 -0300

    refactor: move `display_images` from `screenshot_merger` to `utils`
    refactor: move `display_images` from `screenshot_merger` to `utils`

--- screenshot_merger.py
+++ screenshot_merger.py
@@ -10,8 +10,7 @@ from listeners import KeyboardListener, MouseScrollListener
 from region_selector import RegionSelector
 
 import mss
-from PIL import Image, ImageTk
-import tkinter as tk
+from PIL import Image
 import argparse
 import sys
 import time
@@ -26,37 +25,6 @@ def capture_screenshot(monitor):
         sct_img = sct.grab(monitor)
         img = Image.frombytes('RGB', sct_img.size, sct_img.rgb)
         return img
-
-
-# Function to display images in debug mode
-def display_images(new_image, merged_image):
-    root = tk.Tk(sync=False)
-    root.title("Debug Images")
-
-    # Create frames for layout
-    frame_new = tk.Frame(root)
-    frame_new.pack(side="left", padx=10, pady=10)
-
-    frame_merged = tk.Frame(root)
-    frame_merged.pack(side="right", padx=10, pady=10)
-
-    # New Captured Image
-    new_image_tk = ImageTk.PhotoImage(new_image)
-    new_image_label = tk.Label(frame_new, image=new_image_tk) # type: ignore
-    new_image_label.pack()
-    new_image_title = tk.Label(frame_new, text="New Captured Image")
-    new_image_title.pack()
-
-    # Merged Image
-    if merged_image is not None:
-        merged_image_tk = ImageTk.PhotoImage(merged_image)
-        merged_image_label = tk.Label(frame_merged, image=merged_image_tk) # type: ignore
-        merged_image_label.pack()
-        merged_image_title = tk.Label(frame_merged, text="Current Merged Image")
-        merged_image_title.pack()
-
-    # Keep a reference to the images to prevent garbage collection
-    root.mainloop()
 
 
 def merge_images_thread(screenshot_queue, exit_event):
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from loguru import logger
+import tkinter as tk
+from PIL import Image, ImageTk
 
 def setup_logging(log_lvl="DEBUG", options={}):
     file = options.get("file", False)
@@ -31,3 +33,35 @@ def setup_logging(log_lvl="DEBUG", options={}):
         ]
     )  # type: ignore # yapf: disable
 
+
+
+
+# Function to display images in debug mode
+def display_images(new_image, merged_image):
+    root = tk.Tk(sync=False)
+    root.title("Debug Images")
+
+    # Create frames for layout
+    frame_new = tk.Frame(root)
+    frame_new.pack(side="left", padx=10, pady=10)
+
+    frame_merged = tk.Frame(root)
+    frame_merged.pack(side="right", padx=10, pady=10)
+
+    # New Captured Image
+    new_image_tk = ImageTk.PhotoImage(new_image)
+    new_image_label = tk.Label(frame_new, image=new_image_tk) # type: ignore
+    new_image_label.pack()
+    new_image_title = tk.Label(frame_new, text="New Captured Image")
+    new_image_title.pack()
+
+    # Merged Image
+    if merged_image is not None:
+        merged_image_tk = ImageTk.PhotoImage(merged_image)
+        merged_image_label = tk.Label(frame_merged, image=merged_image_tk) # type: ignore
+        merged_image_label.pack()
+        merged_image_title = tk.Label(frame_merged, text="Current Merged Image")
+        merged_image_title.pack()
+
+    # Keep a reference to the images to prevent garbage collection
+    root.mainloop()
